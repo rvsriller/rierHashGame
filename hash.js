73e6e225daad12e48d87c452f626fcb591eddd8c
Riller <rvsriller@gmail.com>
2021-03-20 23:52:36 -0300

    Hash Game

--- hash.js
+++ hash.js
@@ -1,3 +1,8 @@
 var player, winner = null;
 var selectedPlayer = document.getElementById('selectedPlayer');
 var selectedWinner = document.getElementById('selectedWinner');
+
+function changePlayer(currentPlayer) {
+    player = currentPlayer;
+    selectedPlayer.innerHTML = player;
+}
